88c625082547e2b8bc1cb19cbffa1b0f3a567905
Sefali Gupta <sefaligupta99@gmail.com>
2026-01-21 00:44:54 +0530

    Add initial implementation of SortedMatrix class with binary search method

---
+++ SortedMatrix.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class SortedMatrix {
+    int [][] matrix={
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+    int target=6;
+    public static void main (String[] args){
+
+    }
+
+    //search in the row provided
+    static int[] BinarySearch(int[][] matrix,int cStart,int cEnd,int row,int target){
+        while(cStart<=cEnd){
+            int mid=cStart+(cEnd-cStart)/2;
+            if(matrix[row][mid]==target){
+                return new int[]{row,mid};
+            }
+            else if(matrix[row][mid]<target){
+                cStart=mid+1;
+            }
+            else{
+                cEnd=mid-1;
+            }
+            }
+            return new int[]{-1,-1};
+        }
+
+        stat
+    }
+    }
+    
+}
