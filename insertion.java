f88b7e149417ba7df2a2d4af0c46070b41d62fa1
Sefali Gupta <sefaligupta99@gmail.com>
2026-02-01 12:55:40 +0530

    Refactor insertion class structure and fix formatting issues

--- insertion.java
+++ insertion.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
+public class insertion {
 public static void main (String[] args){
     int[] arr={5,4,3,2,1};
     insertionSort(arr);
     System.out.println(Arrays.toString(arr));
 }
-public class insertion {
+
     static void insertionSort(int[] arr){
         for(int i=0;i<arr.length-1;i++){
             for (int j=i+1;j>0;j--){
@@ -19,11 +20,12 @@ public class insertion {
     
         }
     }
-    
-}
+
+
 static void swap(int[] arr,int first,int second){
                 int temp=arr[first];
                 arr[first]=arr[second];
                 arr[second]=temp;
             }
+        }
         
