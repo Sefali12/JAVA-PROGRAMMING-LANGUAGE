19caadc1ca0372c7e398cc933bb0f942631c7536
Sefali Gupta <sefaligupta99@gmail.com>
2026-02-07 23:47:34 +0530

    Add initial implementation of StringBasic class with main method

---
+++ StringBasic.java
@@ -0,0 +1,9 @@
+public class StringBasic {
+    public static void main(String[] args){
+        int[] arr={2,4,6,8};
+        int a=20;
+        String name="java";
+        System.out.println(name);
+    }
+    
+}
