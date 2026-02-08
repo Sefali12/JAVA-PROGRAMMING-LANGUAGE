e7f957a93ed78142b0c4d5e047179d37c42e95fe
Sefali Gupta <sefaligupta99@gmail.com>
2026-02-09 02:54:33 +0530

    Remove comment about changing input in ClimbingStairs class

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,7 +1,7 @@
 public class ClimbingStairs {
 
     public static void main(String[] args) {
-        int n = 5; // change input here
+        int n = 5; 
         System.out.println(climbStairs(n));
     }
 
