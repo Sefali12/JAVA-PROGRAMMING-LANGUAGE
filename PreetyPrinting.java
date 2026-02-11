9944b6d51340161e01b06718137845fde2b3335c
Sefali Gupta <sefaligupta99@gmail.com>
2026-02-11 21:51:08 +0530

    Remove float output line from PreetyPrinting class and retain greeting message

--- PreetyPrinting.java
+++ PreetyPrinting.java
@@ -1,7 +1,10 @@
 public class PreetyPrinting {
     public static void main(String[] args){
         float a=14.347f;
-        System.out.printf("The Float value is %.2f", a);
+        //System.out.printf("The Float value is %.2f", a);
+        System.out.printf("Hello my name is %s and I am %s\n","Sefali","Gupta" );
+
+
     }
     
 }
