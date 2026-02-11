74a83918a2ab3df9e8957571f289445b54d554af
Sefali Gupta <sefaligupta99@gmail.com>
2026-02-11 22:25:29 +0530

    Refactor PreetyPrinting class to comment out float output and retain series generation

--- PreetyPrinting.java
+++ PreetyPrinting.java
@@ -1,8 +1,15 @@
 public class PreetyPrinting {
     public static void main(String[] args){
-        float a=14.347f;
+        //float a=14.347f;
         //System.out.printf("The Float value is %.2f", a);
-        System.out.printf("Hello my name is %s and I am %s\n","Sefali","Gupta" );
+        //System.out.printf("Hello my name is %s and I am %s\n","Sefali","Gupta" );
+        String series="";
+        for(int i=0;i<26;i++){
+            char ch=(char)('a'+ i);
+            series=series+ch;
+
+        }
+        System.out.println(series);
 
 
     }
