1610486d9d36a4b29c9cf110a1fe49bb0bf4ce40
Sefali Gupta <sefaligupta99@gmail.com>
2026-02-13 02:32:09 +0530

    Refactor PreetyPrinting class to use StringBuilder for series generation and comment out unused code

--- PreetyPrinting.java
+++ PreetyPrinting.java
@@ -3,13 +3,14 @@ public class PreetyPrinting {
         //float a=14.347f;
         //System.out.printf("The Float value is %.2f", a);
         //System.out.printf("Hello my name is %s and I am %s\n","Sefali","Gupta" );
-        String series="";
+        //String series="";
+        StringBuilder builder=new StringBuilder();
         for(int i=0;i<26;i++){
             char ch=(char)('a'+ i);
-            series=series+ch;
+            builder.append(ch);
 
         }
-        System.out.println(series);
+        System.out.println(builder.toString());
 
 
     }
