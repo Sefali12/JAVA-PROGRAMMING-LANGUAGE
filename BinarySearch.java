cdb690a4d154c5c409e262286417e3be580a4585
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-06 21:36:03 +0530

    Add BinarySearch class implementation and update NumbersExampleRecursion for clarity

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class BinarySearch {
+/*public class BinarySearch {
     public static void main(String[] args) {
         int[] arr={-14,-12,-5,0,3,7,9,12,15,18,22,27,30};
         int target=22;
@@ -27,5 +27,27 @@ public class BinarySearch {
             return -1;
         
         }
+    }*/
+   public class BinarySearch{
+    public static void main(String[] args){
+        int[] arr={1,2,45,66,78,88};
+        int target=66;
+        System.out.println(search( arr,target,0,arr.length-1));
+
     }
+
+    static int search(int[] arr,int target, int s,int e){
+        if(s>e){
+            return -1;
+        }
+        int mid=s+(e-s)/2;
+        if(target==arr[mid]){
+            return mid;
+        }
+        if(target>arr[mid]){
+            return search(arr,target,mid+1,e);
+        }
+        return search(arr,target,s,mid-1);
+    }
+   }
 
--- NumbersExampleRecursion.java
+++ NumbersExampleRecursion.java
@@ -5,10 +5,11 @@ public class NumbersExampleRecursion {
 
     static void print(int n){
         if(n==5){
+            System.out.println(5);//BASE CONDITION
             return;
         }
-        System.out.println(n);
-        print(n+1);
+        System.out.println(n);//BODY
+        print(n+1);//RECURSIVE CALL
 
     }
     
