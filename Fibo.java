b3dcfcad87c656e5fd7077ce51a6a94af3ac76db
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-09 17:07:06 +0530

    Update fiboformula method to return long type for improved accuracy in Fibonacci calculation

--- Fibo.java
+++ Fibo.java
@@ -4,11 +4,12 @@ public class Fibo {
         for (int i=0;i<11;i++){
             System.out.println(fiboformula(i));
         }
+        System.out.println(fiboformula(50));
     }
 
-    static int fiboformula(int n){
-        return (int)((Math.pow((1 + Math.sqrt(5)), n) - Math.pow((1 - Math.sqrt(5)), n)) 
-        / (Math.pow(2, n) * Math.sqrt(5)));
+    static long fiboformula(int n){
+        //return (int)((Math.pow((1 + Math.sqrt(5)), n) - Math.pow((1 - Math.sqrt(5)), n)) / (Math.pow(2, n) * Math.sqrt(5)));
+        return (long)(Math.pow(((1+Math.sqrt(5))/2),n)/Math.sqrt(5));
     }
     static int fibo(int n){
         if(n<2){
