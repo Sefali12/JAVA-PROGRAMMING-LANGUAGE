56c7ca72aafa9607b73463eaf679be1b459522d5
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-09 16:54:01 +0530

    Add fiboformula method and update main to demonstrate Fibonacci calculation

--- Fibo.java
+++ Fibo.java
@@ -1,11 +1,19 @@
 public class Fibo {
     public static void main(String[] args){
         System.out.println(fibo(6));
+        for (int i=0;i<11;i++){
+            System.out.println(fiboformula(i));
+        }
+    }
+
+    static int fiboformula(int n){
+        return (int)(Math.pow((1+ Math.sqrt(5)),n)-Math.pow((1-Math.sqrt(5)),n))/(2*n)/Math.sqrt(5);
     }
     static int fibo(int n){
         if(n<2){
             return n;
         }
         return fibo(n-1)+fibo(n-2);
+    
     }
 }
