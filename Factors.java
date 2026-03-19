7efc8b12a9c82e2588609d6165fc98f8546de383
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-20 01:38:54 +0530

    Add Factors class to calculate factors of a given number

---
+++ Factors.java
@@ -0,0 +1,11 @@
+public class Factors {
+    public static void main(String[] args){
+        int n=20;
+        System.out.println(factors(n));
+    }
+
+    static void factor(int n){
+        f
+    }
+    
+}
