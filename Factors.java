3fe6ba2deb6356687e233b999683f40367030707
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-20 01:51:53 +0530

    Add factors2 method to calculate factors using square root optimization

--- Factors.java
+++ Factors.java
@@ -2,8 +2,9 @@ public class Factors {
     public static void main(String[] args){
         int n=20;
         factor(n);
+        factors2(n);
     }
-
+    //O(N)
     static void factor(int n){
         for(int i=1;i<=n;i++){
             if(n%i==0){
@@ -11,6 +12,17 @@ public class Factors {
             }
         }
     
+    }
+    //O(sqrt(n))
+    static void factors2(int n){
+        for(int i=1;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                if(n/i==i){
+                    System.out.println(i+" ");
+                }
+                System.out.println(i+" "+ n/i+ " ");
+            }
+        }
     }
     
 }
