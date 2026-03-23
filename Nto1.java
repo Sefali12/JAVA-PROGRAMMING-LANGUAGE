53e7bd07838ba50797b006d0b2aac8ca8243fcda
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-23 21:38:13 +0530

    Refactor main method to call funRev for reverse countdown

--- Nto1.java
+++ Nto1.java
@@ -1,7 +1,7 @@
 public class Nto1 {
     public static void main(String[] args){
         int n =5;
-        fun(n);
+        funRev(n);
     }
     static void fun(int n){
         if(n==0){
@@ -10,6 +10,13 @@ public class Nto1 {
         }
         System.out.println(n);
         fun(n-1);
+    }
+    static void funRev(int n){
+        if(n==0){
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
     }
     
 }
