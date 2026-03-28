a78c6d88bf9ef3e863b0eff9460085804db8d2de
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-28 15:11:42 +0530

    Update linearsearch to correct target value and add findindex and findallindex methods

--- linearsearch.java
+++ linearsearch.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+
 public class linearsearch {
     public static void main(String[] args) {
-        int arr[]={1,34,56,33,88,57};
-        int target=89;
+        int arr[]={1,34,56,88,33,88,57};
+        int target=88;
         //System.out.println(linearsearch(arr,target));
         System.out.println(linearrsearch(arr,target,0));
+        System.out.println(findindex(arr,target,0));
+        findallindex(arr,target,0);
+        System.out.println(list);
 
     }
     static int linearsearch(int[] arr,int target){
@@ -27,7 +32,28 @@ public class linearsearch {
         }
         return (arr[index]==target) || linearrsearch(arr,target,index+1);
     }
-
-    
+    static int findindex(int[] arr,int target,int index){
+        //base condition
+        if(index==arr.length){
+            return -1;
+        }
+        if(arr[index]==target){
+            return index;
+        }
+        else {
+            return findindex(arr,target,index+1);
+    }
+}
+    static ArrayList<Integer> list= new ArrayList<>();
+     static void findallindex(int[] arr,int target,int index){
+        //base condition
+        if(index==arr.length){
+            return ;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        }
+        findallindex(arr,target,index+1);
+    }
 }
 
