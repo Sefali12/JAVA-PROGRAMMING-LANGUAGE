dac778a2607c76c8c40ec97935e14df2f580317f
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-28 14:55:52 +0530

    Update linearsearch to include recursive search method and modify target value

--- linearsearch.java
+++ linearsearch.java
@@ -1,8 +1,9 @@
 public class linearsearch {
     public static void main(String[] args) {
         int arr[]={1,34,56,33,88,57};
-        int target=88;
-        System.out.println(linearsearch(arr,target));
+        int target=89;
+        //System.out.println(linearsearch(arr,target));
+        System.out.println(linearrsearch(arr,target,0));
 
     }
     static int linearsearch(int[] arr,int target){
@@ -19,5 +20,14 @@ public class linearsearch {
 
         return -1;//not found the element
     }
+    static boolean linearrsearch(int[] arr,int target,int index){
+        //base condition
+        if (index==arr.length){
+            return false;
+        }
+        return (arr[index]==target) || linearrsearch(arr,target,index+1);
+    }
+
     
 }
+
