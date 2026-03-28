9d2a90a01e1a9dad7a37f93120eb4c72279cea30
Sefali Gupta <sefaligupta99@gmail.com>
2026-03-29 03:08:50 +0530

    Add BRS class to implement binary search algorithm

---
+++ BRS.java
@@ -0,0 +1,23 @@
+public class BRS {
+    public static void main(String[] args){
+        int[] arr={5,6,7,8,1,2,3};
+        System.out.println(search(arr,8,0,arr.length-1));
+
+    }
+    static int search(int[] arr,int target,int s,int e){
+        if(s>e){
+            return -1;
+        }
+        int mid=s+(e-s)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(arr[mid]>=target && arr[s]<=target){
+            return search(arr,target,s,mid-1);
+        }
+        else{
+            return search(arr,target,mid+1,e);
+        }
+
+    }
+}
