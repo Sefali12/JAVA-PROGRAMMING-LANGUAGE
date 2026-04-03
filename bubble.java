9bbf51b214de0854ee3ba931f6a702bd42dbb272
Sefali Gupta <sefaligupta99@gmail.com>
2026-04-03 16:23:36 +0530

    Add recursive bubble sort implementation and new Triangle class for printing patterns

--- bubble.java
+++ bubble.java
@@ -2,8 +2,10 @@ import java.util.Arrays;
 public class bubble {
     public static void main(String[] args){
         int[] arr={1,4,6,3,7,2,5};
-        bubbleSort(arr);
-        System.out.println(Arrays.toString(arr));
+        //bubbleSort(arr);
+       // System.out.println(Arrays.toString(arr));
+        bubblerecur(arr,arr.length-1,0);
+        System.out.print(Arrays.toString(arr));
 
     }
 
@@ -20,5 +22,25 @@ public class bubble {
             }
         }
     }
+
+    static void bubblerecur(int[] arr,int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            if(arr[c]>arr[c+1]){
+                int temp=arr[c];
+                arr[c]=arr[c+1];
+                arr[c+1]=temp;
+            }
+        
+            bubblerecur(arr,r,c+1);
+        }
+            else{
+                bubblerecur(arr,r-1,0);
+
+
+            }
+        }
+    }
     
-}
