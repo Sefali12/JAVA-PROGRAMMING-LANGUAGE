6f81b5f6ebe8c62b468b74073d2882a9349032ae
Sefali Gupta <sefaligupta99@gmail.com>
2026-04-15 18:03:05 +0530

    Refactor Permutations class: comment out direct permutation call and add method to return permutations as a list

--- Permutations.java
+++ Permutations.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 public class Permutations {
     public static void main(String[] args){
-        permutations("","abc");
+        //permutations("","abc");
+        ArrayList<String> ans=permutationsList("","abc");
+        System.out.println(ans);
         
 
     }
@@ -17,6 +20,25 @@ public class Permutations {
             permutations(f+ch+s,up.substring(1));
 
         }
+        
+    }
+     static ArrayList<String> permutationsList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+            
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> ans=new ArrayList<>();
+
+        for (int i=0;i<=p.length();i++){
+            String f=p.substring(0,i);
+            String s=p.substring(i,p.length());
+            ans.addAll(permutationsList(f+ch+s,up.substring(1)));
+
+        }
+        return ans;
         
     }
     
