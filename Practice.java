d866a035f7d42c40ae6f9c4f998ccabb9c959cd6
Sefali Gupta <sefaligupta99@gmail.com>
2026-05-30 16:51:08 +0530

    Fix array reversal logic in Practice class and add Frequency class as a placeholder

--- Practice.java
+++ Practice.java
@@ -1,8 +1,29 @@
-
-import java.util.Scanner;
-public class Practice {
+import java.util.*;
+public class Practice{
     public static void main(String[] args){
-        Scanner in=new Scanner(System.in);
-        
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int left=0;
+        int right=n-1;
+        while(right>left){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
 
-     
+
+
+    }
+    int[] rev=new arr[n];
+    for
+    for (int x:arr){
+        System.out.print(x+" ");
+
+    }
+}
+}
